a22bb78fe496325117ec95e0fc4e22e497275bca
Akash <87010703+Akash182249@users.noreply.github.com>
2023-08-02 09:51:33 +0530

    python day_3.py

---
+++ python day_3.py
@@ -0,0 +1,20 @@
+#Problem Statement: Write a program that takes two numbers from the user and performs the following operations:
+
+#Requirements:
+
+#1. Add the two numbers and print the result.
+#2. Subtract the second number from the first number and print the result.
+#3. Multiply the two numbers and print the result.
+#4. Divide the first number by the second number and print the result.
+
+num1=input("enter a number")
+num2=input("enter another number")
+add_nums=num1+num2
+subs_nums=num2-num1
+multiply_nums= num1*num2
+divide_nums=num1/num2
+print(add_nums)
+print(subs_nums)
+print(multiply_nums)
+print(divide_nums)
+         
