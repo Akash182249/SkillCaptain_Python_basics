ed8d9076f0dee64dccadf9e80df27006b8dd61e7
Akash <87010703+Akash182249@users.noreply.github.com>
2023-08-05 08:23:20 +0000

    commit python day_5.py in branch SkillCaptain_Python_basics

---
+++ python day_5.py
@@ -0,0 +1,10 @@
+# Write a program using a while loop to print all the numbers from 1 to 10. Create a list of names. Use a for loop to iterate over the list and print a personalized greeting for each name.
+num = 1
+while num < 11:  
+
+    print(num)
+    num += 1
+a=["Rohan", "Akash", "Nice"]
+for element in a:
+    print("Hey,",element)
+
